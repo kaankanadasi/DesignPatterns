756b866d3e6dd998588c384dd671b7cd525de370
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2024-01-20 17:04:14 +0300

    Create Interfaces.java

---
+++ DesignPatternBasics/Interfaces.java
@@ -0,0 +1,24 @@
+// coupling - how much a class is dependent on another class, for efficient coupling we use interfaces
+// interface - a contract that specifies he capabilities that a class should provide 
+// abstraction - reduce complexity by hiding unnecessary details, örneğin main'd ekullanmayacağın metodları private yap
+
+public interface TaxCalculator {
+    float calculateTax();
+}
+ 
+public class TaxCalculator2023 implements TaxCalculator {
+    @Override
+    public float calculateTax() {
+        return 1;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        TaxCalculator calculator = getCalculator();
+        calculator.calculateTax();
+    }
+    public static TaxCalculator getCalculator() {
+        return new TaxCalculator2023();
+    }
+}
