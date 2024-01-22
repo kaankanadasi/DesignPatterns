1804c13c644877a02d8664c2985167503bf229bd
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2024-01-22 15:16:11 +0300

    Create Animal.java

---
+++ BenimAnladığım/4-Polymorphism/Example/Animal.java
@@ -0,0 +1,17 @@
+// superclass/parent class
+public class Animal {
+    private String name;
+    private String color;
+
+    public void eat() {
+        System.out.println("yum yum");
+    }
+    // getter method
+    public String getName() {
+        return name;
+    }
+    // setter method
+    public void setName(String name) {
+        this.name = name;
+    }
+}
