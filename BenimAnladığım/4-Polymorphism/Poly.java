0bfd457de6470f2a53c2f5ed3e0a512497446c74
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2024-01-22 15:16:02 +0300

    Create Poly.java

---
+++ BenimAnladığım/4-Polymorphism/Poly.java
@@ -0,0 +1,26 @@
+/*
+ * source: https://www.youtube.com/watch?v=m_MQYyJpIjg
+ * Fundemental Concepts of Object-Oriented programming
+ *  - Abstraction
+ *  - Encapsulation
+ *  - Inheritnace 
+ *  - Polymorphism
+*/
+
+// Object -  thing that you want to store and process data about 
+
+// Abstraction - to simplify reality 
+// For objects that we want to integrate in our program the important thing is the relevant datas and 
+// tasks that we want to perform with those datas 
+
+// Class - a template for creating objects, deifnes the attributes and methods of the obejct
+// Class is used to create many objects of the same type, class is sometimes referred to as type
+// Each objeect is an instance of a class in the computers memory 
+// Instantiation - Creating an object rom a class 
+
+// Encapsulation - hiding data and complexity 
+// her şeye erişmemiz gerekmiyor -> private/public keyword'leriyle istediğimiz method ya da attrbute'ları accessible belirliyebiliriz
+
+// Inheritance - a class can erive its methods nd properties from another class
+
+// Polynorphism - a class can implement an inherited method in its own way
