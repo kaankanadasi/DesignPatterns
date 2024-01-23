b535f9edfcc12cac300d9a33f28c83f24097b198
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2024-01-23 12:26:16 +0300

    Create Multilevel.java

---
+++ BenimAnladığım/4-Polymorphism/Multilevel.java
@@ -0,0 +1,28 @@
+// source: https://www.javatpoint.com/runtime-polymorphism-in-java
+
+class Animal {  
+    void eat() {
+        System.out.println("eating");
+    }  
+}  
+
+class Dog extends Animal {  
+    void eat() {
+        System.out.println("eating fruits");
+    }  
+}  
+
+class BabyDog extends Dog {  
+    void eat() { 
+        System.out.println("drinking milk"); 
+    }  
+    public static void main(String args[]) {  
+        Animal a1,a2,a3;  
+        a1 = new Animal();  
+        a2 = new Dog();  
+        a3 = new BabyDog();  
+        a1.eat(); // eating
+        a2.eat(); // eating fruits
+        a3.eat(); // drinking Milk
+    }  
+}  
