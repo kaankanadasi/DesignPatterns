a5946ab0bd88eaae4148db5b223521f8e2a10193
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2024-01-23 12:26:27 +0300

    Update Cat.java

--- BenimAnladığım/4-Polymorphism/Example/Cat.java
+++ BenimAnladığım/4-Polymorphism/Example/Cat.java
@@ -5,12 +5,16 @@ public class Cat extends Animal {
     public void eat() {
         System.out.println("num num num");
     }
+    @Override
+    public void makeNoise() {
+        System.out.println("meow meow");
+    }
 
     // method overloading is a kind of polymorphism  - methods that share the same name but have different parameters 
     @Overload
     public void eat(int numTimes, String food) { 
         for(int i = 0; i < numTimes; i++) {
-            System.out.println("yum yum" + food);
+            System.out.print("yum yum " + food + " - ");
         }
     }
     public int getBreed() {
