b2005640969e1c74f7e696a21e6b7ca1e46e8128
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2024-01-23 12:26:18 +0300

    Update Main.java

--- BenimAnladığım/4-Polymorphism/Example/Main.java
+++ BenimAnladığım/4-Polymorphism/Example/Main.java
@@ -1,19 +1,27 @@
 // source1 [John]: https://www.youtube.com/watch?v=jhDUxynEQRI
-// source2 [Yale graduate]; https://www.youtube.com/watch?v=qqYOYIVrso0
+// source2 [John]: https://www.youtube.com/watch?v=HpuH7n9VOYk&t=94s
+// source3 [Yale graduate]; https://www.youtube.com/watch?v=qqYOYIVrso0
 // poly = many, morph = form
 
 public class Main {
     public static void main(String[] args) {
         Animal myAnimal = new Animal();
-        myAnimal.eat();
+        myAnimal.eat(); // yum yum
+        doAnimalStuff(myAnimal); // I'm an animal
 
         Dog myDog1 = new Dog();
-        // Dog is an Animal, polymorphism allows this dynamic relationship
-        Animal myDog2 = new Dog(); 
-        myDog1.eat();
+        myDog1.eat(); // nice
+        myDog1.growl(); // Grrrrrr
+        doAnimalStuff(myDog1); // Grrrrrr
+
+        Animal myDog2 = new Dog(); // upcasting, Dog is-a Animal
+        myDog2.eat(); // yum yum
+        myDog2.growl(); // COMPILATION ERROR!!! THIS DOES NOT WORK BECAUSE ANIMAL CLASS DOES NOT HAVE bark() METHOD
+        doAnimalStuff(myDog2); // Grrrrrr
 
         Cat myCat = new Cat();
-        myCat.eat(3, "manti");
+        myCat.eat(2, "manti"); // yum yum manti - yum yum manti
+        doAnimalStuff(myCat); // meow meow
 
         Animal[] Kingdom = new Animal[2];
         Kingdom[0] = myDog1;
@@ -21,6 +29,14 @@ public class Main {
 
         for(Animal a : Kingdom) {
             a.eat();
+        }
+    }
+
+    public static void doAnimalStuff(Animal animal) {
+        animal.makeNoise();
+        if (animal instanceof Dog) {
+            Dog myDog = (Dog) animal; // downcasting
+            myDog.growl();
         }
     }
 }
