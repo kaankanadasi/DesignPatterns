ca62d4aba55b2923f9635450c1dbdc9ab26d6a48
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2024-02-18 15:20:36 +0300

    Update Classes.java

--- DesignPatternBasics/Classes.java
+++ DesignPatternBasics/Classes.java
@@ -7,24 +7,25 @@
  * in the paranthesis we have the parameters of the method (a string array called args), if we execute this program from the 
    command line and pass a few arguments, we can capture those arguments using the string array
 */
-
-public class Main {
-    public static void(String[] args) {
-        User user = new User("Mosh"); 
-        user.sayHello();
+public class Classes {
+    public class Main {
+        public static void main(String[] args) {
+            User user = new User("Mosh"); 
+            user.sayHello();
+        }
     }
-}
 
-public class User {
-    // Fields (attributes)
-    public String name;
+    public class User {
+        // Fields (attributes)
+        public String name;
 
-    // Constructor - a method that gets called when we create a new instance of a class
-    public User(Stirg name) {
-        this.name = name; // set the name field of the current object to the value of the name parameter
+        // Constructor - a method that gets called when we create a new instance of a class
+        public User(String name) {
+            this.name = name; // set the name field of the current object to the value of the name parameter
+        } 
+        // Methods
+        public void sayHello() {
+            System.out.println("Hi, my name is " + name);
+        }
     } 
-    // Methods
-    public void sayHello() {
-        System.out.println("Hi, my name is " + name);
-    }
-} 
+}
