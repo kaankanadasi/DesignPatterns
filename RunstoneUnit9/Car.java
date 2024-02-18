7e1aa4119a99cb39ba1b128b71f49dc9cf580df5
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2024-02-18 15:20:44 +0300

    Create Car.java

---
+++ RunstoneUnit9/Car.java
@@ -0,0 +1,35 @@
+public class Car {
+    private int fuel;
+
+    public Car() {
+        fuel = 0;
+    }
+    public Car(int g) {
+        fuel = g;
+    }
+
+    public void addFuel() {
+        fuel++;
+    }
+    public void display() {
+        System.out.print(fuel + " ");
+    }
+
+    public static void main(String[] args) {
+        Car car = new Car(5);
+        Car fastCar = new RaceCar(5);
+        car.display();
+        car.addFuel();
+        car.display();
+        fastCar.display();
+        fastCar.addFuel();
+        fastCar.display();
+        // prints: 5 6 10 11
+    }
+}
+
+class RaceCar extends Car {
+    public RaceCar(int g) {
+        super(2 * g);
+    }
+}
