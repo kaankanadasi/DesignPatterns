fae539580d03a6538d169b7ef21860f5d820832d
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2024-02-18 15:20:42 +0300

    Create Book.java

---
+++ RunstoneUnit9/Book.java
@@ -0,0 +1,16 @@
+public class Book {
+    private String ISBN = "something";
+
+    public String getISBN() {
+        return ISBN;
+    }
+    // constructors, fields, and other methods not shown
+}
+
+public class Dictionary extends Book {
+    public String getDefinition() {
+        // implementation not shown
+    }
+}
+
+//b.getDefintion();  -  At compile time the declared type is Book and the Book class does not have or inherit a getDefintion method.
