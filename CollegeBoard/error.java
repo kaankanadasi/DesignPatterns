c5900fbc60f34f6417f8e0cfc786f30bcebf3747
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2024-02-20 11:26:00 +0300

    Create error.java

---
+++ CollegeBoard/error.java
@@ -0,0 +1,14 @@
+package CollegeBoard;
+
+public class error {
+    ArrayList<int> intList = new ArrayList<int>();
+    // compile error: we cannot use a primitive (int) as the type parameter in a generic ArrayList
+}
+
+
+
+/*
+ * Merge sort uses the "divide and conquer" approach to sort an array. This will end up being more efficient than insertion sort in 
+ * the case where we have long unordered array. However if we have a very small almost sorted array, then insertion sort will 
+ * outperform merge sort.
+*/
