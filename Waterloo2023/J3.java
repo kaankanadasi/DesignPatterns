3aace500970d4ede812e4ef38207411fde5d884f
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2024-02-20 23:56:56 +0300

    Create J3.java

---
+++ Waterloo2023/J3.java
@@ -0,0 +1,14 @@
+package Waterloo2023;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class J3 {
+    Scanner scan = new Scanner(System.in);
+    int numDays = scan.nextInt();
+    // 2D array
+    String[] days = new String[numDays];
+
+    for(int i=0; i<numDays; i++) {
+        String lines = scan.nextLine();
+    }
+}
