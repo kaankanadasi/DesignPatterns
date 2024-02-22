a1a2af3a25392081972de38ac5a4f3612438a5ea
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2024-02-22 13:02:04 +0300

    Create J2.java

---
+++ Waterloo/J2.java
@@ -0,0 +1,23 @@
+package Waterloo;
+
+import java.util.Scanner;
+
+public class J2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int D = scan.nextInt(); // Dusa's starting size
+        int R = D;
+
+        while(scan.hasNext()) {
+            int yobi = scan.nextInt();
+
+            if(D > yobi) {
+                R += yobi; // if Dusa is bigger it eats
+            } else {
+                break; // if encountered big Yobi break
+            }
+        }
+        System.out.println(R);
+        scan.close();  
+    }
+}
