b89089ec6dbad637f0eb5c797c12914a79d6cfe3
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2024-02-22 14:08:11 +0300

    Update Dog.java

--- BenimAnladığım/4-Polymorphism/Example/Dog.java
+++ BenimAnladığım/4-Polymorphism/Example/Dog.java
@@ -9,7 +9,6 @@ public class Dog extends Animal {
     public void growl() {
         System.out.println("Grrrrrr");
     }
-    @Override
     public void makeNoise() {
         System.out.println("wof wof");
     }
