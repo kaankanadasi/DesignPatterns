4e2080d5537961f43a096ec74beba7ee4f205646
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2024-02-24 23:15:08 +0300

    Update J4.java

--- Waterloo/Waterloo2024/J4.java
+++ Waterloo/Waterloo2024/J4.java
@@ -1,6 +1,8 @@
 package Waterloo.Waterloo2024;
 import java.util.Scanner;
 
+// https://cccgrader.com/getproblem.php?fid=1101456&authcode=79add7606a1afc4075ad21ac0a4b5636
+
 public class J4 {
     public static void main(String[] args) {
         // quiet key -> not displayed
